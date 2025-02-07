5d43593d20f9ad1ccdd37fb78082d8803bfffa7d
Andrey ABRAMOV <abramov.andre@yandex.ru>
2025-02-07 17:08:34 +0300

    Add files via upload

--- page_02.py
+++ page_02.py
@@ -11,7 +11,7 @@ import numpy as np
 import pandas as pd
 #import matplotlib.pyplot as plt
 #import seaborn as sns
-import plotly.express as px
+#import plotly.express as px
 
 #st.title('Страница 01')
 
